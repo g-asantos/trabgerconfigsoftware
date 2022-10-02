72d91751ea2c6658e4a91b1788b844b3fcfd6698
Pedro Tigre <97975218+pedrotigre@users.noreply.github.com>
2022-10-02 13:03:54 -0300

    feat(sessao): adicionar singleton/getInstance()

--- src/sessao/Sessao.java
+++ src/sessao/Sessao.java
@@ -6,4 +6,15 @@ public class Sessao {
     private static Sessao instance;
     private Usuario usuarioLogado;
     
+    public static Sessao getInstance() {
+        if (instance == null) {
+            instance = new Sessao();
+        }
+
+        return instance;
+    }
+
+    private Sessao() {}
+
+    
 }
