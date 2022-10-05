f1946a75ee5742620e6a79325d6b5f3ef9f43dec
Pedro Tigre <97975218+pedrotigre@users.noreply.github.com>
2022-10-05 12:50:31 -0300

    feat(estatistica): Adicionando atributos usuarioRepository e autorizacaoRepository

---
+++ src/estatistica/Estatistica.java
@@ -0,0 +1,9 @@
+package estatistica;
+
+import repository.UsuarioRepository;
+import repository.AutorizacaoRepository;
+
+public class Estatistica {
+    private UsuarioRepository usuarioRepository;
+    private AutorizacaoRepository autorizacaoRepository;
+}
