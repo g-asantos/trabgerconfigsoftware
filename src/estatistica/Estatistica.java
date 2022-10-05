a6680519b340ea35bcff979808c7f43f23d69e75
Pedro Tigre <97975218+pedrotigre@users.noreply.github.com>
2022-10-05 13:09:07 -0300

    feat(estatistica): Adicionando metodos numeroMedicos e numeroPacientes

--- src/estatistica/Estatistica.java
+++ src/estatistica/Estatistica.java
@@ -2,8 +2,19 @@ package estatistica;
 
 import repository.UsuarioRepository;
 import repository.AutorizacaoRepository;
+import dominio.TipoUsuario;
 
 public class Estatistica {
     private UsuarioRepository usuarioRepository;
     private AutorizacaoRepository autorizacaoRepository;
+    
+    /* Retorna o numero de medicos */
+    public int numeroMedicos() {
+        return usuarioRepository.listarPorTipo(TipoUsuario.MEDICO).size();
+    }
+
+    /* Retorna o numero de pacientes */
+    public int numeroPacientes() {
+        return usuarioRepository.listarPorTipo(TipoUsuario.PACIENTE).size();
+    }
 }
